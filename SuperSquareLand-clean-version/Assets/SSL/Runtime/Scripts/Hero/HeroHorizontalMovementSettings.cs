90a1828d37dbd3146c36e5a2e5be9e7b17a971bb
Ambre-Maurugeon <129891903+Ambre-Maurugeon@users.noreply.github.com>
2024-04-22 12:30:55 +0200

    V3

---
+++ SuperSquareLand-clean-version/Assets/SSL/Runtime/Scripts/Hero/HeroHorizontalMovementSettings.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class HeroHorizontalMovementSettings
+{
+    public float acceleration = 20f;
+    public float speedMax=5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
