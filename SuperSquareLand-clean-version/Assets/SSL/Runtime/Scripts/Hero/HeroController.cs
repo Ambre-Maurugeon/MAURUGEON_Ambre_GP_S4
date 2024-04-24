22f55dd8d0df42c920362c8562bde5b0b0e0ffb7
Ambre-Maurugeon <129891903+Ambre-Maurugeon@users.noreply.github.com>
2024-04-24 11:19:41 +0200

    V11
    jump buffer

--- SuperSquareLand-clean-version/Assets/SSL/Runtime/Scripts/Hero/HeroController.cs
+++ SuperSquareLand-clean-version/Assets/SSL/Runtime/Scripts/Hero/HeroController.cs
@@ -5,17 +5,35 @@ public class HeroController : MonoBehaviour
     [Header("Entity")]
     [SerializeField] private HeroEntity _entity;
 
+    [Header("Jump Buffer")]
+    [SerializeField] private float _jumpBufferDuration =0.2f;
+    private float _jumpBufferTimer = 0f;
+
     [Header("Debug")]
     [SerializeField] private bool _guiDebug = false;
 
+    private void Start(){
+        _CancelJumpBuffer();
+    }
+
     private void Update (){
+        _UpdateJumpBuffer();
+
         _entity.SetMoveDirX(GetInputMoveX());
 
         if (Input.GetKeyDown(KeyCode.Space)){
             if(_entity.IsTouchingGround && !_entity.IsJumping){
                 _entity.JumpStart();
+            } else {
+                _ResetJumpBuffer();
             }
         } 
+
+        if(IsJumpBufferActive()){
+            if(_entity.IsTouchingGround && !_entity.IsJumping){
+                _entity.JumpStart();
+            }
+        }
 
         if(_entity.IsJumpImpulsing){
             if(!Input.GetKey(KeyCode.Space) && _entity.IsJumpMinDurationReached){
@@ -24,7 +42,6 @@ public class HeroController : MonoBehaviour
         }
 
         if(Input.GetKeyDown(KeyCode.E)){
-            //_entity.Dash();
             _entity.Dash(_entity.dashSettings);
         }
     }
@@ -40,12 +57,31 @@ public class HeroController : MonoBehaviour
         return InputMoveX;
     }
 
+    private void _ResetJumpBuffer(){
+        _jumpBufferTimer=0f;
+    }
+
+    private void _UpdateJumpBuffer(){
+        if(!IsJumpBufferActive()) return;
+        _jumpBufferTimer += Time.deltaTime;
+    }
+
+    private bool IsJumpBufferActive(){
+        return _jumpBufferTimer < _jumpBufferDuration;
+    }
+
+    private void _CancelJumpBuffer(){
+        _jumpBufferTimer = _jumpBufferDuration;
+    }
+
+//Debug
     private void OnGUI()
     {
         if (!_guiDebug) return;
 
         GUILayout.BeginVertical(GUI.skin.box);
         GUILayout.Label(gameObject.name);
+        GUILayout.Label("Jump Buffer Timer = " + _jumpBufferTimer);
         GUILayout.EndVertical();
     }
 }
